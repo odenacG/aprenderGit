384090592e59de94a166d53ead6075e1a64c52cb
odenacG <gisele.canedo@gmail.com>
2023-08-27 10:00:20 -0300

    Altera na Branch1: print na tela

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,8 @@
 public class HelloWorld {
   public static void main(String[] args) {
+    System.out.println("************** print na tela *****************************");
     System.out.println("Hello World! Na Branch1");
+    System.out.println("Ainda na Branch1. Mais uma alteração");
+    System.out.println("------------fim print na tela -------------------------------");
   }
 }
